67ab1fe07df1131f8e9ec8d74e4f03c637a4e4a5
Sverri Reinert <sverrireinert@gmail.com>
2021-11-29 13:48:34 +0100

    nav bar implemented

---
+++ src/main/resources/static/scripts/global.js
@@ -0,0 +1,23 @@
+const baseURL = "http://localhost:8080";
+
+
+const navBar = document.getElementById("nav-bar");
+
+navBar.innerHTML=`
+
+<a href="../html/overview.html"><img class="logo" src="../pngegg.png" alt="logo"></a>
+<div>
+    <a href="../html/overview.html">
+        <p>Overblik</p>
+    </a>
+    <a href="../html/cars.html">
+        <p>Biler</p>
+    </a>
+    <a href="../html/employees.html">
+        <p>Ansatte</p>
+    </a>
+</div>
+<div>
+
+</div>
+`;
