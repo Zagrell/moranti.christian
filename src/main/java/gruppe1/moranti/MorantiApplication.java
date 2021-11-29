717fbafe4d8785e8b6a655449d99b91798a47f25
Sverri Reinert <sverrireinert@gmail.com>
2021-11-29 10:46:37 +0100

    Initial commit

---
+++ src/main/java/gruppe1/moranti/MorantiApplication.java
@@ -0,0 +1,13 @@
+package gruppe1.moranti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorantiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorantiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe1/moranti/MorantiApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe1.moranti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorantiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
