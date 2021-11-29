70b5c9f52e79214a3f9c95960d4a088beba3ad7b
Sverri Reinert <sverrireinert@gmail.com>
2021-11-29 16:41:50 +0100

    púsh

--- src/main/java/gruppe1/moranti/contollers/OverviewContoller.java
+++ src/main/java/gruppe1/moranti/contollers/OverviewContoller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class JSApp {
+public class OverviewContoller {
 
     @GetMapping("/")
     public String index(){
