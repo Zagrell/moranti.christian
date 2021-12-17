cec49d93c1eebb924fe0d9e19e0982240c2cdef1
Delamir <td_christian@hotmail.com>
2021-12-17 10:28:21 +0100

    Css iz

--- src/main/java/gruppe1/moranti/restcontrollers/WaitingLists.java
+++ src/main/java/gruppe1/moranti/restcontrollers/WaitingLists.java
@@ -8,6 +8,8 @@ import gruppe1.moranti.repositories.WaitingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 public class WaitingLists {
